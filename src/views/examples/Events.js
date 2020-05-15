c4cc6e34d8a6bdfe8e75775680831d8d16029640
Arslan Sajid <arslansajid951@gmail.com>
2020-05-15 18:49:40 +0500

    Events page updated

--- src/views/examples/Events.js
+++ src/views/examples/Events.js
@@ -1,24 +1,118 @@
 import React from "react";
-import { Container, Row, Col } from "reactstrap";
 import BookingCard from "../../components/BookingCard";
+import {
+    Button,
+    Label,
+    FormGroup,
+    Input,
+    NavItem,
+    NavLink,
+    Nav,
+    TabContent,
+    TabPane,
+    Container,
+    Row,
+    Col
+} from "reactstrap";
 
-const Events = () => {
+const Events = (props) => {
     document.documentElement.classList.remove("nav-open");
-    const count = 5;
+
+    const [activeTab, setActiveTab] = React.useState("1");
+
+    const toggle = tab => {
+        if (activeTab !== tab) {
+            setActiveTab(tab);
+        }
+    };
+
+    let count = 3;
     return (
         <div className="section blue-bg">
             <Container className="py-5">
                 <Row>
-                    <Col lg="12">
+                    <Col lg="12 d-flex align-items-center">
+                        <div onClick={() => props.history.goBack()} className="back-button">
+                            <i
+                                className="fa fa-2x fa-arrow-left text-white mr-3"
+                            />
+                        </div>
                         <h1 className="py-3 text-white">Events</h1>
+
                     </Col>
                     <Col className="ml-auto mr-auto" lg="6">
-                        {[...Array(count)].map((e, index) => {
-                            return (
-                                <BookingCard />
-                            )
-                        })}
+                        <Button className="w-100 mb-5" color="danger" size="lg">SCAN</Button>
                     </Col>
+                    <Col lg="12">
+                        <div className="nav-tabs-navigation">
+                            <div className="nav-tabs-wrapper">
+                                <Nav role="tablist" tabs>
+                                    <NavItem>
+                                        <NavLink
+                                            className={activeTab === "1" ? "active" : ""}
+                                            onClick={() => {
+                                                toggle("1");
+                                            }}
+                                        >
+                                            Total
+                                        </NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink
+                                            className={activeTab === "2" ? "active" : ""}
+                                            onClick={() => {
+                                                toggle("2");
+                                            }}
+                                        >
+                                            Present
+                                        </NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink
+                                            className={activeTab === "3" ? "active" : ""}
+                                            onClick={() => {
+                                                toggle("3");
+                                            }}
+                                        >
+                                            Departed
+                                        </NavLink>
+                                    </NavItem>
+                                </Nav>
+                            </div>
+                        </div>
+                    </Col>
+                    {/* Tab panes */}
+                    {
+                        activeTab === "1"
+                            ?
+                            <Col className="ml-auto mr-auto" lg="6">
+                                <h3 className="text-white mb-3">Total Users: 90</h3>
+                                {[...Array(count)].map((e, index) => {
+                                    return (
+                                        <div key={index}>
+                                            <BookingCard />
+                                        </div>
+                                    )
+                                })}
+                            </Col>
+                            :
+                            activeTab === "2"
+                            ?
+                            <Col className="ml-auto mr-auto" lg="6">
+                                <h1 className="text-white">No records found!</h1>
+                            </Col>
+                            :
+                            <Col className="ml-auto mr-auto" lg="6">
+                                <h3 className="text-white mb-3">Total Users: 87</h3>
+                                {[...Array(count)].map((e, index) => {
+                                    return (
+                                        <div key={index}>
+                                            <BookingCard />
+                                        </div>
+                                    )
+                                })}
+                            </Col>
+                        }
                 </Row>
             </Container>
         </div>
